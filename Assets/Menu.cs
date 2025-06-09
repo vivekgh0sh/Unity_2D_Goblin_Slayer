d8683dd4cf94f751614dd4f7cf0d7f4222f850db
Vivek Ghosh <vivekghosh165@gmail.com>
2025-06-09 13:42:56 +0530

    MENU UI

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
